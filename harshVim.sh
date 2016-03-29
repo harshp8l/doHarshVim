98cbcb6b6c4dea53fdb6509c5b00222d795558a2
Harsh Patel <harshpatel081296@yahoo.com>
2016-03-28 18:59:39 -0700

    done

---
+++ harshVim.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/harshp8l/Vimcolors.git
+git clone https://github.com/harhsp8l/Vimrc.git
+cd ~/Vimrc
+./placeVimrc.sh
+cd ~/Vimcolors
+./placeVimcolors.sh
+cd ~
+rm -rf ~/Vimrc ~/Vimcolors ~/doHarshVim
+rm harshVim.sh
+
